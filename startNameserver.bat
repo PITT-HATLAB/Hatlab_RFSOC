06cf14a7dd9b7ad18d0c68e7f71ed667288d2037
CHAO ZHOU <44282719+hatlabcz@users.noreply.github.com>
2022-05-30 18:38:48 -0400

    updated nameserver batch file, make it use the local ipv4 address instead of hostname

--- startNameserver.bat
+++ startNameserver.bat
@@ -5,6 +5,11 @@ IF exist C:\ProgramData\Anaconda3 (call C:\ProgramData\Anaconda3\Scripts\activat
 :: Start a Pyro4 nameserver with pc hostname
 SET PYRO_SERIALIZERS_ACCEPTED=pickle
 SET PYRO_PICKLE_PROTOCOL_VERSION=4
-for /f "delims=" %%i in ('hostname') do pyro4-ns -n "%%i" -p 8888
+:: find IPV4 address of the ethernet card on local network, assuming each PC has two ethernet cards, and the one with 136.142 address is for pitt network
+for /f "tokens=1-2 delims=:" %%a in ('ipconfig^|find "IPv4"^|find /v ": 136.142"') do set LOCALIPV4=%%b  
+pyro4-ns -n %LOCALIPV4% -p 8888
+
+:: using hostname doesn't guarantee that the IPv4 of local network will be used.
+:: for /f "delims=" %%i in ('hostname') do pyro4-ns -n "%%i" -p 8888 
 
 PAUSE
