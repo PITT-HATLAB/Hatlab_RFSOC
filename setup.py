046696f8c786d3bb94df3e6f73ece7edcbda74a0
CHAO ZHOU <44282719+hatlabcz@users.noreply.github.com>
2023-05-31 09:29:05 -0400

    updated setup.py.
    The toml was not quite working.. will come back to it later. For now, let's use the simple setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 from distutils.core import setup
 
-setup(name='Hatlab_RFSOC',
+setup(
+      name='Hatlab_RFSOC',
       version='0.0.1',
-      packages=['Hatlab_RFSOC'])
+      packages=['Hatlab_RFSOC'],
+
+      python_requires='>=3.7, <4',
+
+      install_requires=[
+            "Pyro4"
+      ],
+)
