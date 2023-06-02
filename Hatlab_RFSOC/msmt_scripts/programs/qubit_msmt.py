48011715215013da283d657bf189113991d811c9
CHAO ZHOU <44282719+hatlabcz@users.noreply.github.com>
2023-06-02 02:02:05 -0400

    Update qubit_msmt.py
    to use the new `get_gen_reg`, `new_gen_reg`, register.`set_to` functions

--- Hatlab_RFSOC/msmt_scripts/programs/qubit_msmt.py
+++ Hatlab_RFSOC/msmt_scripts/programs/qubit_msmt.py
@@ -1,8 +1,9 @@
-from typing import List
+from typing import List, Callable
+
+import numpy as np
 from qick.averager_program import AveragerProgram
 from qick.qick_asm import QickProgram
 from Hatlab_RFSOC.core.averager_program import NDAveragerProgram, QickSweep, QubitMsmtMixin
-
 
 
 class CavityResponseProgram(QubitMsmtMixin, NDAveragerProgram):
@@ -25,6 +26,27 @@ class CavityResponseProgram(QubitMsmtMixin, NDAveragerProgram):
                      wait=True,
                      syncdelay=self.us2cycles(self.cfg["relax_delay"]))
 
+
+class CavityResponseProgram_tanh(QubitMsmtMixin, NDAveragerProgram):
+    def initialize(self):
+        cfg = self.cfg
+        self.res_ch = self.cfg["gen_chs"]["res_drive"]["ch"]
+
+        # set readout pulse registers
+        self.add_waveform_from_cfg("res_drive", "res_tanh")
+        self.set_pulse_params_auto_gen_type("res_drive", **cfg["res_pulse_config"])
+
+        self.synci(200)  # give processor some time to configure pulses
+
+    def body(self):
+        self.measure(pulse_ch=self.res_ch,
+                     adcs=self.ro_chs,
+                     pins=[0],
+                     adc_trig_offset=self.cfg["adc_trig_offset"],
+                     wait=True,
+                     syncdelay=self.us2cycles(self.cfg["relax_delay"]))
+
+
 class PrepareQubitCavityResponseProgram(QubitMsmtMixin, NDAveragerProgram):
     def initialize(self):
         cfg = self.cfg
@@ -32,6 +54,7 @@ class PrepareQubitCavityResponseProgram(QubitMsmtMixin, NDAveragerProgram):
         self.qubit_ch = self.cfg["gen_chs"]["q_drive"]["ch"]
 
         # set readout pulse registers
+        self.add_waveform_from_cfg("res_drive", "res_tanh")
         self.set_pulse_params_auto_gen_type("res_drive", **cfg["res_pulse_config"])
 
         # set qubit prob pulse registers
@@ -41,14 +64,19 @@ class PrepareQubitCavityResponseProgram(QubitMsmtMixin, NDAveragerProgram):
 
         self.synci(200)  # give processor some time to configure pulses
 
+
     def body(self):
         cfg = self.cfg
         # drive and measure
+        sel_msmt = cfg.get("sel_msmt", False)
+        if sel_msmt:
+            self.add_prepare_msmt("q_drive", cfg["q_pulse_cfg"], "res_drive", syncdelay=cfg["msmt_leakout_time"])
+
         self.set_pulse_params("q_drive", style="arb", waveform="q_gauss", phase=0,
                                 freq=cfg["q_pulse_cfg"]["ge_freq"], gain=cfg["prepare_gain"])
 
         self.pulse(ch=self.qubit_ch)  # play gaussian pulse
-        self.sync_all(self.us2cycles(0.05))  # align channels and wait 50ns
+        self.sync_all(self.us2cycles(0.1))  # align channels and wait 50ns
 
         self.measure(pulse_ch=self.res_ch,
                      adcs=self.ro_chs,
@@ -56,6 +84,8 @@ class PrepareQubitCavityResponseProgram(QubitMsmtMixin, NDAveragerProgram):
                      adc_trig_offset=self.cfg["adc_trig_offset"],
                      wait=True,
                      syncdelay=self.us2cycles(self.cfg["relax_delay"]))
+
+
 
 class PulseSpecProgram(QubitMsmtMixin, NDAveragerProgram):
     def initialize(self):
@@ -67,13 +97,13 @@ class PulseSpecProgram(QubitMsmtMixin, NDAveragerProgram):
         self.set_pulse_params_auto_gen_type("res_drive", **cfg["res_pulse_config"])
 
         # set qubit prob pulse registers
-        self.add_waveform_from_cfg("q_drive", "q_gauss") # for post sel
+        self.add_waveform_from_cfg("q_drive", "q_gauss")
         self.set_pulse_params("q_drive", style="const", length=cfg["prob_length"],
                                 phase=0, freq=cfg["f_start"], gain=cfg["prob_gain"])
 
         # add prob pulse frequency sweep
-        self.q_r_freq = self.get_reg("q_drive", "freq")
-        self.q_r_freq_update = self.new_reg("q_drive", reg_type="freq")
+        self.q_r_freq = self.get_gen_reg("q_drive", "freq")
+        self.q_r_freq_update = self.new_gen_reg("q_drive", reg_type="freq")
         self.add_sweep(QickSweep(self, self.q_r_freq_update, cfg["f_start"], cfg["f_stop"], cfg["f_expts"]))
 
         self.sync_all(self.us2cycles(1))  # give processor some time to configure pulses
@@ -88,9 +118,10 @@ class PulseSpecProgram(QubitMsmtMixin, NDAveragerProgram):
         # set pulse shape to prob pulse
         self.set_pulse_params("q_drive", style="const", length=cfg["prob_length"],
                                  phase=0, freq=cfg["f_start"], gain=cfg["prob_gain"])
-        self.mathi(self.q_r_freq.page, self.q_r_freq.addr, self.q_r_freq_update.addr, '+', 0)  # set the updated freq value
+        self.q_r_freq.set_to(self.q_r_freq_update)  # set the updated freq value
         self.pulse(ch=self.qubit_ch)  # play prob pulse
-        self.sync_all(self.us2cycles(0.05))  # align channels and wait 50ns
+        self.sync_all(self.us2cycles(0.05))  # ali
+        # gn channels and wait 50ns
 
         # --- msmt
         self.measure(pulse_ch=self.res_ch,
@@ -108,6 +139,10 @@ class AmplitudeRabiProgram(QubitMsmtMixin, NDAveragerProgram):
         self.qubit_ch = self.cfg["gen_chs"]["q_drive"]["ch"]
 
         # set readout pulse registers
+        try:
+            self.add_waveform_from_cfg("res_drive", "res_tanh")
+        except Exception:
+            pass
         self.set_pulse_params_auto_gen_type("res_drive", **cfg["res_pulse_config"])
 
 
@@ -117,8 +152,8 @@ class AmplitudeRabiProgram(QubitMsmtMixin, NDAveragerProgram):
                                 freq=cfg["q_pulse_cfg"]["ge_freq"], gain=cfg["g_start"])
 
         # add qubit pulse gain sweep
-        self.q_r_gain = self.get_reg("q_drive", "gain")
-        self.q_r_gain_update = self.new_reg("q_drive", init_val=cfg["g_start"])
+        self.q_r_gain = self.get_gen_reg("q_drive", "gain")
+        self.q_r_gain_update = self.new_gen_reg("q_drive", init_val=cfg["g_start"])
         self.add_sweep(QickSweep(self, self.q_r_gain_update, cfg["g_start"], cfg["g_stop"], cfg["g_expts"]))
 
         self.sync_all(self.us2cycles(1))  # give processor some time to configure pulses
@@ -131,7 +166,7 @@ class AmplitudeRabiProgram(QubitMsmtMixin, NDAveragerProgram):
             self.add_prepare_msmt("q_drive", cfg["q_pulse_cfg"], "res_drive", syncdelay=cfg["msmt_leakout_time"])
 
         # drive and measure
-        self.mathi(self.q_r_gain.page, self.q_r_gain.addr, self.q_r_gain_update.addr, '+', 0)  # set the updated gain value
+        self.q_r_gain.set_to(self.q_r_gain_update)  # set the updated gain value
         self.pulse(ch=self.qubit_ch)  # play gaussian pulse
         self.sync_all(self.us2cycles(0.05))  # align channels and wait 50ns
         # --- msmt
@@ -159,7 +194,7 @@ class T1Program(QubitMsmtMixin, NDAveragerProgram):
                                 freq=cfg["q_pulse_cfg"]["ge_freq"], gain=cfg["q_pulse_cfg"]["pi_gain"])
 
         # add t_proc waiting time sweep
-        self.t_r_wait = self.new_reg("q_drive", init_val=cfg["t_start"], reg_type="time", tproc_reg=True)
+        self.t_r_wait = self.new_gen_reg("q_drive", init_val=cfg["t_start"], reg_type="time", tproc_reg=True)
         self.add_sweep(QickSweep(self, self.t_r_wait, cfg["t_start"], cfg["t_stop"], cfg["t_expts"]))
 
         self.sync_all(self.us2cycles(1))  # give processor some time to configure pulses
@@ -193,6 +228,10 @@ class T2RProgram(QubitMsmtMixin, NDAveragerProgram):
         self.qubit_ch = self.cfg["gen_chs"]["q_drive"]["ch"]
 
         # set readout pulse registers
+        try:
+            self.add_waveform_from_cfg("res_drive", "res_tanh")
+        except Exception:
+            pass
         self.set_pulse_params_auto_gen_type("res_drive", **cfg["res_pulse_config"])
 
 
@@ -202,7 +241,7 @@ class T2RProgram(QubitMsmtMixin, NDAveragerProgram):
                                 freq=cfg["q_pulse_cfg"]["t2r_freq"], gain=cfg["q_pulse_cfg"]["pi2_gain"])
 
         # add t_proc waiting time sweep
-        self.t_r_wait = self.new_reg("q_drive", init_val=cfg["t_start"], reg_type="time", tproc_reg=True)
+        self.t_r_wait = self.new_gen_reg("q_drive", init_val=cfg["t_start"], reg_type="time", tproc_reg=True)
         self.add_sweep(QickSweep(self, self.t_r_wait, cfg["t_start"], cfg["t_stop"], cfg["t_expts"]))
 
         self.sync_all(self.us2cycles(1))  # give processor some time to configure pulses
@@ -247,7 +286,7 @@ class T2EProgram(QubitMsmtMixin, NDAveragerProgram):
                                 freq=cfg["q_pulse_cfg"]["ge_freq"], gain=cfg["q_pulse_cfg"]["pi2_gain"])
 
         # add t_proc waiting time sweep
-        self.t_r_wait = self.new_reg("q_drive", init_val=cfg["t_start"], reg_type="time", tproc_reg=True)
+        self.t_r_wait = self.new_gen_reg("q_drive", init_val=cfg["t_start"], reg_type="time", tproc_reg=True)
         self.add_sweep(QickSweep(self, self.t_r_wait, cfg["t_start"]/2, cfg["t_stop"]/2, cfg["t_expts"]))
 
         self.sync_all(self.us2cycles(1))  # give processor some time to configure pulses
@@ -301,8 +340,8 @@ class EfPulseSpecProgram(QubitMsmtMixin, NDAveragerProgram):
                                  phase=0, freq=cfg["f_start"], gain=cfg["prob_gain"])
 
         # add prob pulse frequency sweep
-        self.q_r_freq = self.get_reg("q_drive", "freq")
-        self.q_r_freq_update = self.new_reg("q_drive", reg_type="freq")
+        self.q_r_freq = self.get_gen_reg("q_drive", "freq")
+        self.q_r_freq_update = self.new_gen_reg("q_drive", reg_type="freq")
         self.add_sweep(QickSweep(self, self.q_r_freq_update, cfg["f_start"], cfg["f_stop"], cfg["f_expts"]))
 
         self.sync_all(self.us2cycles(1))  # give processor some time to configure pulses
@@ -322,7 +361,8 @@ class EfPulseSpecProgram(QubitMsmtMixin, NDAveragerProgram):
 
         # set pulse to prob pulse
         self.set_pulse_params("q_drive", style="const", length=cfg["prob_length"], phase=0, freq=cfg["f_start"], gain=cfg["prob_gain"])
-        self.mathi(self.q_r_freq.page, self.q_r_freq.addr, self.q_r_freq_update.addr, '+', 0)  # set the updated freq value
+        self.q_r_freq.set_to(self.q_r_freq_update)  # set the updated freq value
+
         self.pulse(ch=self.qubit_ch)  # play prob pulse
         self.sync_all(self.us2cycles(0.05))  # align channels and wait 50ns
 
@@ -356,8 +396,8 @@ class EfRabiProgram(QubitMsmtMixin, NDAveragerProgram):
                               gain=cfg["q_pulse_cfg"]["pi_gain"])
 
         # add ef pulse gain sweep
-        self.q_r_gain = self.get_reg("q_drive", "gain")
-        self.q_r_gain_update = self.new_reg("q_drive")
+        self.q_r_gain = self.get_gen_reg("q_drive", "gain")
+        self.q_r_gain_update = self.new_gen_reg("q_drive")
         self.add_sweep(QickSweep(self, self.q_r_gain_update, cfg["g_start"], cfg["g_stop"], cfg["g_expts"]))
 
         self.sync_all(self.us2cycles(1))  # give processor some time to configure pulses
@@ -379,11 +419,11 @@ class EfRabiProgram(QubitMsmtMixin, NDAveragerProgram):
         # set and play ef gaussian pulse
         self.set_pulse_params("q_drive", style="arb", waveform="q_gauss", phase=0, freq=cfg["q_pulse_cfg"]["ef_freq"],
                               gain=cfg["q_pulse_cfg"]["pi_gain"])
-        self.mathi(self.q_r_gain.page, self.q_r_gain.addr, self.q_r_gain_update.addr, '+', 0)  # set the updated gain value
+        self.q_r_gain.set_to(self.q_r_gain_update)  # set the updated gain value
         self.pulse(ch=self.qubit_ch)  # play ef pulse
         self.sync_all(self.us2cycles(0.05))  # align channels and wait 50ns
 
-        if cfg["flip_back_g"]: # for better final msmt resolution (usually the cavity is driven at best g/e separation)
+        if cfg.get("flip_back_g", True): # for better final msmt resolution (usually the cavity is driven at best g/e separation)
             self.set_pulse_params("q_drive", style="arb", waveform="q_gauss", phase=0, freq=cfg["q_pulse_cfg"]["ge_freq"],
                                   gain=cfg["q_pulse_cfg"]["pi_gain"])
             self.pulse(ch=self.qubit_ch)
